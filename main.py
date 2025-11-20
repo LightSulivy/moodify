63d89fa9c29e229b030581a644dd8c2cb4e72744
LightSulivy <bast91.teisseire@gmail.com>
2025-11-20 14:47:24 +0100

    Ajout d'un scaler

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import kagglehub
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import os
 
 # Télécharger la derniere vesion du dataset
@@ -18,13 +19,13 @@ if csv_files:
     csv_file_path = os.path.join(path, csv_files[0])
     df = pd.read_csv(csv_file_path)
     print(f"Loaded dataset: {csv_files[0]}")
-    print(df.head())
+    print(df.columns)
 else:
     print("No CSV file found in the downloaded dataset.")
 
 
-# Séparer les features et la value objectif
-x = df.drop(columns=['labels'])
+# Separer les features et les valeurs objectifs
+x = df.drop(columns=['labels',"Unnamed: 0"])
 y = df["labels"]
 
 print(x.head())
@@ -34,3 +35,14 @@ print(y.head())
 x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=0.2)
 
 
+
+#Debut du pre-traitements des donnees (preprocessing)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(x_train)
+X_test_scaled = scaler.transform(x_test)
+
+
+# Note : Le résultat est un array Numpy. Pour le remettre en DataFrame (optionnel pour la visibilité) :
+X_train_scaled_df = pd.DataFrame(X_train_scaled, columns=x.columns)
+
+print(X_train_scaled_df.head())
